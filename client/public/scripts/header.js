bf2d9885b7cb0d87e5efa718d46f405fb6a1c17c
Johnny <jac188@duke.edu>
2023-09-16 15:12:02 -0400

    set up node express environment

---
+++ client/public/scripts/header.js
@@ -0,0 +1,8 @@
+const header = document.querySelector('header')
+const headerContainer = document.createElement('div')
+headerContainer.className = 'header-container'
+
+const headerContainer = document.createElement('div')
+headerContainer.className = 'header-left'
+
+
