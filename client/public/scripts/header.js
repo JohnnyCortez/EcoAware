7d58f24e43632446224b6f282e4c277497b75a57
Johnny <jac188@duke.edu>
2023-09-22 22:47:10 -0400

    fixed 404 issue

--- client/public/scripts/header.js
+++ client/public/scripts/header.js
@@ -1,29 +1,29 @@
 const header = document.querySelector('header')
+
 const headerContainer = document.createElement('div')
 headerContainer.className = 'header-container'
 
 const headerLeft = document.createElement('div')
 headerLeft.className = 'header-left'
 
-const headerLogo = document.createElement("img");
-// Set the src attribute
-headerLogo.src = "logo.png"; // Replace "logo.png" with the actual path to your image\
+const headerLogo = document.createElement('img')
+headerLogo.src = '/logo.png'
 
-const headerTitle = document.createElement("h1");
-// Set the text content of the h1 element
-headerTitle.textContent = "UnEarthed";
+const headerTitle = document.createElement('h1')
+headerTitle.textContent = 'UnEarthed'
 
 headerLeft.appendChild(headerLogo)
 headerLeft.appendChild(headerTitle)
 
-const headerRight = document.createElement('div')
-headerRight.className = 'header-right'
-
 const headerButton = document.createElement('Home')
 headerButton.textContent = 'Home'
-headerButton.addEventListener('click', function handleClick(event) {
+    
+headerButton.addEventListener('click', (event) => {
   window.location = '/'
 })
+
+const headerRight = document.createElement('div')
+headerRight.className = 'header-right'
 
 headerRight.appendChild(headerButton)
 
@@ -31,5 +31,6 @@ headerContainer.appendChild(headerLeft)
 headerContainer.appendChild(headerRight)
 
 header.appendChild(headerContainer)
+
 
 
